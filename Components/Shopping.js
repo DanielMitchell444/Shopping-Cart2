4d9a837d64399bec9c52e147c03989fc75fa3a66
Daniel Mitchell <49624246+DanielMitchell444@users.noreply.github.com>
2024-03-08 13:52:43 -0800

    Add files via upload

--- Components/Shopping.js
+++ Components/Shopping.js
@@ -1,12 +1,25 @@
 import React from "react";
 
-const Shopping = (props) => {
-   const [name] = props.data
+const Shopping = ({data}) => {
  return(
  <div>
-    {props.data.map(data => {
-      <h1>{name}</h1>
-    })}
+  {data.map(data => {
+   return(
+ <div class = "container2">
+   <div class = "card">
+    <div class = "productImage">
+    <img src = {data.image} alt = {data.name} class = "images" />
+    </div>
+    <h3>{data.title}</h3>
+    <p>${data.price}</p>
+    <button 
+    class = "adds"
+    
+    >Add to Cart</button>
+   </div>
+   </div>
+   )
+  })}
  </div>
  )
 }
